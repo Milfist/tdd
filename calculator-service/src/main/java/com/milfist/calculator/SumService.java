53365a5ccd615a1f081200599fe9e6768c27f3a5
Milfist <milfist@gmail.com>
2021-01-27 15:41:53 +0100

    fix: codification error. Internal Class deleted. Method renamed.

--- calculator-service/src/main/java/com/milfist/calculator/SumService.java
+++ calculator-service/src/main/java/com/milfist/calculator/SumService.java
@@ -6,18 +6,14 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+@Path("/v1")
 public class SumService {
 
-  @Path("/v1")
-  public class Sum {
-
-    @GET
-    @Path("/sum")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String greeting(@QueryParam("number1") Integer number1, @QueryParam("number2") Integer number2) {
-      return "sum = " + (Integer.sum(number1, number2));
-    }
+  @GET
+  @Path("/sum")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String sum(@QueryParam("number1") Integer number1, @QueryParam("number2") Integer number2) {
+    return "sum = " + (Integer.sum(number1, number2));
   }
 
 }
