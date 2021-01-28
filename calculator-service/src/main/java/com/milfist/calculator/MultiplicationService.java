c00b9ec8823088ba8d1e52d068ead6657e75d888
Milfist <milfist@gmail.com>
2021-01-28 13:31:03 +0100

    feat: use case 2, multiplication service

---
+++ calculator-service/src/main/java/com/milfist/calculator/MultiplicationService.java
@@ -0,0 +1,19 @@
+package com.milfist.calculator;
+
+import org.jboss.resteasy.annotations.jaxrs.QueryParam;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1")
+public class MultiplicationService {
+  @GET
+  @Path("/multiplication")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String sum(@QueryParam("number1") Integer number1, @QueryParam("number2") Integer number2) {
+    return "product = " + (number1 * number2);
+  }
+
+}
